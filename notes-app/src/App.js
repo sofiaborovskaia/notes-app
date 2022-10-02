dc81f837e50c0da7402723f1831e57c048899be9
Sofia Borovskaia <sofiaborovskaia@gmail.com>
2022-10-02 18:27:31 +0100

    Add a delete notes option

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -41,6 +41,11 @@ function App() {
 		});
 	}
 
+	function deleteNote(event, noteId) {
+		event.stopPropagation();
+		setNotes((prevNotes) => prevNotes.filter((note) => note.id !== noteId));
+	}
+
 	function findCurrentNote() {
 		return (
 			notes.find((note) => {
@@ -58,6 +63,7 @@ function App() {
 						currentNote={findCurrentNote()}
 						setCurrentNoteId={setCurrentNoteId}
 						newNote={createNewNote}
+						deleteNote={deleteNote}
 					/>
 					{currentNoteId && notes.length > 0 && (
 						<Editor currentNote={findCurrentNote()} updateNote={updateNote} />
